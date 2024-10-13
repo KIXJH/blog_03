b1077a168897fb31078c9bbcf34ac7c6ea688c49
KIXJH <97908895+KIXJH@users.noreply.github.com>
2024-10-13 21:07:49 +0800

    Update MyBlogApplication.java
    http://localhost:8888/admin/login

--- src/main/java/com/site/blog/my/core/MyBlogApplication.java
+++ src/main/java/com/site/blog/my/core/MyBlogApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //springboot注解
 // java基础 反射 注解
+//http://localhost:8888/admin/login
 @MapperScan("com.site.blog.my.core.dao")
 @SpringBootApplication
 public class MyBlogApplication {
