a16cfabb68227aefd16ee91c94428031c746e22a
KIXJH <97908895+KIXJH@users.noreply.github.com>
2024-10-13 21:06:15 +0800

    Update MyBlogWebMvcConfigurer.java
    update config implements of MyBlogWebMvcConfigurer

--- src/main/java/com/site/blog/my/core/config/MyBlogWebMvcConfigurer.java
+++ src/main/java/com/site/blog/my/core/config/MyBlogWebMvcConfigurer.java
@@ -4,11 +4,12 @@ import com.site.blog.my.core.interceptor.AdminLoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
 
 @Configuration
-public class MyBlogWebMvcConfigurer {
+public class MyBlogWebMvcConfigurer implements WebMvcConfigurer {
     @Resource
     private AdminLoginInterceptor adminLoginInterceptor;
 
